6a751c6212a1018c42d673c8f59a1fefaa31cdef
Jay Park <devpark.0x1c@gmail.com>
2021-06-23 12:26:24 +0900

    Fix: Resolve Conflict - Fix: 피드백 사항 수정 완료

---
+++ src/components/Map/Map.js
@@ -0,0 +1,39 @@
+/*global kakao*/
+import React, { useEffect } from 'react';
+
+const Map = props => {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(props.address, function (result, status) {
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position: coords,
+        });
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <div id="map" style={{ width: '100vw', height: '300px' }}></div>
+    </div>
+  );
+};
+
+export default Map;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,12 +1,11 @@
 const theme = {
+  Blue: '#0075EF',
   DarkBlue: '#0275EF',
   Gray: '#B1B1B0',
   HotPink: '#FF0066',
   LightGray: '#F4F4F4',
-  LightWhite: '#eeeeee',
   Red: '#FF4033',
   SignitureColor: '#3397FF',
-  Yellow: '#fbe850',
   fontColor: '#000000',
   fontFamily: "'Noto Sans KR', sans-serif;",
 };
