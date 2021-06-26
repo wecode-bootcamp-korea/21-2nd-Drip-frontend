ff097426bf637064078f717b24e7e30dcfdfea18
Jay Park <devpark.0x1c@gmail.com>
2021-06-26 13:50:37 +0900

    Fix: 피드백 사항 반영완료 - Add: 카카오톡 로그인 버튼을 이미지로 대체 - Add: 문구 수정 - Fix: 피드백 수정사항 반영완료 - Fix: 로그인 환경변수 설정 - Fix: index.html 빈값 수정 완료

---
+++ src/pages/Register/KakaoLogin.js
@@ -0,0 +1,48 @@
+import { LOGIN_API } from '../../config';
+
+const kakaoLogin = history => {
+  window.Kakao.Auth.login({
+    success: res => {
+      const authToken = res.access_token;
+
+      window.Kakao.API.request({
+        url: '/v2/user/me',
+        data: {
+          property_keys: [
+            'properties.nickname',
+            'properties.profile_image',
+            'kakao_account.email',
+          ],
+        },
+        success: res => {
+          fetch(`${LOGIN_API}/users/signin`, {
+            method: 'POST',
+            headers: {
+              Authorization: authToken,
+            },
+            body: JSON.stringify({
+              name: res.properties.nickname,
+              profile_image: res.properties.profile_image,
+              email: res.kakao_account.email,
+            }),
+          })
+            .then(res => res.json())
+            .then(res => {
+              if (res.token) {
+                localStorage.setItem('Token', res.token);
+                alert('로그인 되었습니다.');
+                history.push('/');
+              } else {
+                alert('다시 확인해 주세요.');
+              }
+            });
+        },
+      });
+    },
+    fail: function (err) {
+      console.log('에러', err);
+    },
+  });
+};
+
+export default kakaoLogin;
