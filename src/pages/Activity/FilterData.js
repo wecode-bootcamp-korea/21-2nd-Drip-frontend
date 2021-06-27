ca500b60b123209befd83bc51a1568851796ed03
shinmiyoung <smy910102@gmail.com>
2021-06-27 13:37:33 +0900

    Add: 액티비티, 배움 리스트 페이지 기능 구현 - Fixed: 1차 리뷰 수정사항 반영 완료 - Fixed: 1차 리뷰 수정사항 추가 수정 완료

---
+++ src/pages/Activity/FilterData.js
@@ -0,0 +1,8 @@
+const CATEGORY_FILTER = [
+  { id: 1, value: '', name: '필터', queryString: '' },
+  { id: 2, value: 'popular', name: '인기순', queryString: '/1' },
+  { id: 3, value: 'highPrice', name: '높은 가격순', queryString: '/2' },
+  { id: 4, value: 'lowPrice', name: '낮은 가격순', queryString: '/3' },
+];
+
+export default CATEGORY_FILTER;
