1203b475f13e30db9ff56f3a50c9e329dfda246d
Jay Park <devpark.0x1c@gmail.com>
2021-06-30 18:25:13 +0900

    Fix: Resolved Conflict - Fix: 피드백 사항 수정완료

--- src/pages/Bookmark/Bookmark.js
+++ src/pages/Bookmark/Bookmark.js
@@ -1,54 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import BottomNav from '../../components/BottomNav/BottomNav';
+import HeaderSearch from '../../components/HeaderSearch/HeaderSearch';
+import Loading from '../../components/Loading/Loading';
 import MainCard from '../../components/MainCard/MainCard';
 import { API } from '../../config';
 import { commonLayOut } from '../../styles/mixin';
 
 const Bookmark = () => {
   const [bookmarkData, setBookMarkData] = useState([]);
-  const {
-    product_id: id,
-    name: title,
-    price,
-    discount,
-    image: thumbnail,
-    new: isNew,
-    hot: isHot,
-    rating: grade,
-  } = bookmarkData;
+  const [isLoading, SetIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${API}/orders?page=1offset=0&limit=4`)
-      .then(res => res.json())
-      .then(res => setBookMarkData(res));
+    const authToken = localStorage.getItem('Token');
+    try {
+      axios(`${API}/orders/bookmark?offset=0&limit=4`, {
+        headers: {
+          Authorization: authToken,
+        },
+      }).then(res => setBookMarkData(res.data));
+    } catch (error) {
+      console.error(error);
+    }
+    SetIsLoading(false);
   }, []);
 
+  if (isLoading) return <Loading />;
   return (
     <BookMarkWrapper>
+      <HeaderSearch />
       <CardListWrapper>
-        <MainCard
-          id={id}
-          title={title}
-          price={price}
-          listPrice={parseInt(price) * (1 - discount)}
-          thumbnail={thumbnail}
-          isNew={isNew}
-          isHot={isHot}
-          grade={grade}
-        />
+        {bookmarkData.result &&
+          bookmarkData.result.map((data, index) => {
+            return (
+              <MainCard
+                key={index}
+                id={data.product_id}
+                title={data.name}
+                price={parseInt(
+                  data.price * (1 - data.discount)
+                ).toLocaleString()}
+                listPrice={parseInt(data.price).toLocaleString()}
+                thumbnail={data.image}
+                isNew={data.new}
+                isHot={data.hot}
+                grade={data.rating.slice(0, 3)}
+              />
+            );
+          })}
       </CardListWrapper>
+      <BottomNav />
     </BookMarkWrapper>
   );
 };
 
 const BookMarkWrapper = styled.section`
   ${commonLayOut}
-  padding: 10px 20px;
 `;
 
 const CardListWrapper = styled.section`
+  padding: 10px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+
+  &:nth-child(1, 2) {
+    padding-top: 20px;
+  }
 `;
 
 export default Bookmark;
